3d681ebb329ddec0cd918d04c0b7f8e3308445f2
Sneha-dotnet-training <Snehapatil3939@gmail.com>
2023-10-16 17:49:44 +0530

    Add project files.

---
+++ JWTAutho/Interface/IEmployees.cs
@@ -0,0 +1,13 @@
+﻿using JWTAutho.Models;
+namespace JWTAutho.Interface
+{
+    public interface IEmployees
+    {
+        public List<Employee> GetEmployeeDetails();
+        public Employee GetEmployeeDetails(int id);
+        public void AddEmployee(Employee employee);
+        public void UpdateEmployee(Employee employee);
+        public Employee DeleteEmployee(int id);
+        public bool CheckEmployee(int id);
+    }
+}
